c02066e6e16f3adbbcf6b66efb53b68bd581f235
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-12-17 09:31:20 +0500

    Create EmailApp

---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+public class EmailApp {
+
+    public static void main(String[] args) {
+
+        Email em1 = new Email("Sohail", "Karmani");
+
+        System.out.println(em1.ShowInfo());
+
+    }
+
+}
